760caab79d03f0d0b7e38a26cc8c1d2d622addda
Vitor Matheus de Moraes <vitor.matheusm@gmail.com>
2024-11-07 22:32:21 -0300

    feat: classificador de herois

---
+++ desafio1.js
@@ -0,0 +1,23 @@
+let nome = "VitorMoraes99";
+let experiencia = 7200;
+let nivel;
+
+if (experiencia < 1000) {
+    nivel = "Ferro";
+} else if (experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
